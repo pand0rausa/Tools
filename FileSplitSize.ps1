585e6c178057fc0e61010e5781b7a65501fa6f88
pand0rausa <pand0ra.usa@gmail.com>
2018-11-04 00:57:17 -0600

    Create FileSplitSize.ps1

---
+++ FileSplitSize.ps1
@@ -0,0 +1,18 @@
+$upperBound = 50MB # calculated by Powershell
+$ext = "log"
+$rootName = "log_"
+
+$reader = new-object System.IO.StreamReader("C:\Exceptions.log")
+$count = 1
+$fileName = "{0}{1}.{2}" -f ($rootName, $count, $ext)
+while(($line = $reader.ReadLine()) -ne $null)
+{
+    Add-Content -path $fileName -value $line
+    if((Get-ChildItem -path $fileName).Length -ge $upperBound)
+    {
+        ++$count
+        $fileName = "{0}{1}.{2}" -f ($rootName, $count, $ext)
+    }
+}
+
+$reader.Close()
